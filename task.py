b261293211f40b4be7f8e5b99b077162748e04c8
justan00b <52006538+bashcube@users.noreply.github.com>
2020-01-01 00:31:29 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from tkinter import font
+import os
+
+GIT_REPO = "https://github.com/bashcube/"
+GMAIL_ACC = "https://gmail.com"
+SONG_LIST = "/$USER/Music/"
+
+def opengit():
+	os.system("firefox --new-window -url "+GIT_REPO)
+
+def openmail():
+	os.system("firefox --new-window -url "+GMAIL_ACC)
+
+def playsongs():
+	os.system("mpg123 -z -vC "+SONG_LIST+"*.mp3")
+
+def Continue():
+	return ''
+
+def main():
+	window = Tk()
+	window.geometry("300x230")
+	window.title("Automating Tasks...")
+
+	fonts = font.Font(family='Helvetica', size=11, weight='bold')
+
+	Frame1 = LabelFrame(window, text="TASK 1.")
+	Frame1.pack(fill="both", expand="yes")
+
+	btn1 = Button(Frame1, text = 'OPEN GIT REPOSITORY',font = fonts, command = opengit, height = 2, width = 33)
+	btn1.place(x = 0, y = 0)
+
+	Frame2 = LabelFrame(window, text = "TASK 2.")
+	Frame2.pack(fill="both", expand = "yes")
+
+	btn1 = Button(Frame2, text = 'OPEN GMAIL ACCOUNT',font = fonts, command = openmail,height = 2, width = 33)
+	btn1.place( x = 0, y = 0)
+
+	Frame3 = LabelFrame(window, text = "TASK 3.")
+	Frame3.pack(fill="both", expand = "yes")
+
+	btn1 = Button(Frame3, text = 'PLAY MUSIC ', font = fonts, command = playsongs, height = 2, width = 33)
+	btn1.place( x = 0, y = 0)
+
+	window.mainloop()
+
+main()
